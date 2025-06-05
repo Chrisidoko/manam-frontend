46ca339c3909a1f9f57b6feb747fbcc80e86d29a
Chris Idoko <chrisidoko0017@gmail.com>
2025-06-05 01:54:40 +0100

    services

--- src/app/PersistentLayout.tsx
+++ src/app/PersistentLayout.tsx
@@ -31,19 +31,21 @@ export default function PersistentLayout({
       {/* Main content area */}
       <main className="flex-grow">{children}</main>
 
-      <div className="fixed bottom-6 right-6 z-50">
-        <div className="bg-green-500 text-white w-fit px-3 py-2 hover:bg-green-600 cursor-pointer rounded-2xl shadow-lg">
-          <a
-            href="https://wa.me/2349160422222?text=Hi%20there%2C%20I%20have%20a%20question"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-2"
-          >
-            <span>Chat on WhatsApp</span>
-            <RiWhatsappLine className="text-white text-xl" />
-          </a>
+      {showLayout && (
+        <div className="fixed bottom-6 right-6 z-50">
+          <div className="bg-green-500 text-white w-fit px-3 py-2 hover:bg-green-600 cursor-pointer rounded-2xl shadow-lg">
+            <a
+              href="https://wa.me/2349160422222?text=Hi%20there%2C%20I%20have%20a%20question"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2"
+            >
+              <span>Chat on WhatsApp</span>
+              <RiWhatsappLine className="text-white text-xl" />
+            </a>
+          </div>
         </div>
-      </div>
+      )}
 
       {/* Footer */}
       {showLayout && <Footer />}
